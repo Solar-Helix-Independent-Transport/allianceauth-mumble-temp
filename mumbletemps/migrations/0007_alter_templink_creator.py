29b61969b3897e7ff6984a83fc79efc8cef20ebc
Aaron Kable <aaronkable@gmail.com>
2026-03-28 10:17:23 +0800

    fix migrations to <v5

--- mumbletemps/migrations/0007_alter_templink_creator.py
+++ mumbletemps/migrations/0007_alter_templink_creator.py
@@ -6,7 +6,7 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("eveonline", "0020_alter_eveallianceinfo_options_and_more"),
+        ("eveonline", "0018_alter_evecharacter_alliance_name_and_more"),
         ("mumbletemps", "0006_tempuser_character_id"),
     ]
 
